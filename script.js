03d9bbee7866d1f75e32c4f6d6f0d4972094a15c
wassemsamir <wasemsamir92@gmail.com>
2021-01-14 12:51:42 +0400

    capitalize string

--- script.js
+++ script.js
@@ -1,54 +1,40 @@
-/*var myStraing ="i love javascript and python",
-    myArray = myStraing.split(" ");
-    console.log(myArray);
-// make straing text and convert to array 
+// add class color in the local storge
+document.body.classList.add(localStorage.getItem("pageColor")||"red");
 
-for ( i = 0 ; i < myArray.length ; i=i+1){
-    console.log(myArray[i])
-}
-// create loop in the array 
-*/
+// get the list 
+var el = document.querySelectorAll('.container li'),
+ //container to push the new class 
+    colorList = [];
+    //loop on all the datacolor in the list
+ for ( i = 0 ;i < el.length ; i++){
+     //push the class name in the container
+    colorList.push(el[i].getAttribute("data-color"));
+    // click event and run funcation to add the new class in the body 
+    el[i].addEventListener('click',function(){
+        //remove the old class
+        document.body.classList.remove(...colorList)
+        // add the new class 
+        document.body.classList.add(this.getAttribute('data-color'))
+        // add the previous color in the local storge 
+        localStorage.setItem("pageColor",this.getAttribute('data-color'))
+    })
+ }
 
-//  Math.ceil
-//  Math.floor
-//  Math.round
-//  Math.max
-//  Math.min
-//  Math.random
  
-// Regular Experssion 
-// /pattern/ attributes 
-// search | Replace | Match | Split | Test
 
-// Attributes List 
-// [i] => case insensitive
-// [g] => global search 
-// [m] => multi line search 
-// brackets 
-// [   ]all character
-// [^  ]not all character
-// [a-e]small range 
-// [A-E]capital range
-// [1-9] numbers 
 
-//---------------------
-
-document.body.classList.add(localStorage.getItem("pageColor")|| "red");
-var li = document.querySelectorAll(".container li");
-var classesList= [];
-//loop on elment
-for ( var i = 0 ; i <li.length ;i++){
-
-    classesList.push(li[i].getAttribute("data-color"));
-
-    li[i].addEventListener('click', function(){
-        document.body.classList.remove(...classesList);
-
-        document.body.classList.add(this.getAttribute('data-color'));
-        localStorage.setItem("pageColor",this.getAttribute("data-color"));
-    },
-    false
-);
+/// function to captilze the string
+function textConvert(string){
+    //convert the string to opject
+    var oldArray= string.split(' ');
+    //container for output loop 
+    var newArray = [];
+    //loop on the string 
+    for (i = 0 ; i< oldArray.length; i++){
+        //pust the loop resulat in the array and concatenat the opject 
+        newArray.push(oldArray[i].charAt(0).toUpperCase()+ oldArray[i].slice(1));
+    }
+    // return the funcation 
+    return newArray.join(" ")
 }
-
-console.log(localStorage.getItem('pageColor'));
+console.log(textConvert("i love javascript and pyhton"));
